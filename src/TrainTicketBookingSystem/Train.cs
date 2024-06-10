c53eff86559d3052000c1f687f5468eb3d7f4de6
PorriDG3 <gn02053231@gmail.com>
2024-06-10 16:12:57 +0800

    feat: Add Train class for Train Ticket Booking System

---
+++ src/TrainTicketBookingSystem/Train.cs
@@ -0,0 +1,23 @@
+using TrainTicketBookingSystem.MDD;
+
+namespace TrainTicketBookingSystem;
+
+public class Train : Aggregate<Guid>
+{
+    protected Train(Guid id, int seats, List<Location> locations, Date date) : base(id)
+    {
+        Seats = seats;
+        _locations = locations;
+        Date = date;
+    }
+
+    public int Seats { get; private set; }
+    private List<Location> _locations;
+    public IReadOnlyList<Location> Locations => _locations.AsReadOnly();
+    public Date Date { get; private set; }
+
+    public static Train Create(Guid id, int seats, List<Location> locations, Date date)
+    {
+        return new Train(id, seats, locations.ToList(), date);
+    }
+}
